27483c76e78da063038afda0c552bbe8d7dc2fb8
elzioauditore <133705924+elzioauditore@users.noreply.github.com>
2026-05-05 07:17:06 -0500

    Create sumar.py

---
+++ sumar.py
@@ -0,0 +1,9 @@
+
+while True: 
+
+    print("---- SUMA ----- \n")
+    entrada = int(input("Numero 1 : "))
+
+    entrada2 = int(input("Numero 2 : "))
+
+    print("Resultado : ", entrada + entrada2)
